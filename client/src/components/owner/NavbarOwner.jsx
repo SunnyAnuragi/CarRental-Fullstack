d8b1b13e3971b929b6643e2dd1cfa739a05f500c
sunny anuragi <sunnyanuragi946@gmail.com>
2026-08-01 19:20:54 +0530

    Recolor custom logo to royal blue and scale up navbar dimensions

--- client/src/components/owner/NavbarOwner.jsx
+++ client/src/components/owner/NavbarOwner.jsx
@@ -10,7 +10,7 @@ const NavbarOwner = () => {
   return (
     <div className='flex items-center justify-between px-6 md:px-10 py-4 text-gray-500 border-b border-borderColor relative transition-all'>
       <Link to='/'>
-        <img src={assets.logo} alt="" className="h-9 md:h-11 w-auto object-contain"/>
+        <img src={assets.logo} alt="" className="h-11 md:h-14 w-auto object-contain"/>
       </Link>
       <p>Welcome, {user?.name || "Owner"}</p>
     </div>
